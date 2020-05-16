890b15bb23eacf3dfaf5480b37ec3711c4998f34
arielmerin <arielmerino@ciencias.unam.mx>
2020-05-15 21:54:55 -0500

    Modificación al estado del widget como StatefulWidget
    Hicimos que la clase principal heredera de dicha clase par ver que también se le puede dar otro comportamiento a los widgets

--- first_proj/lib/main.dart
+++ first_proj/lib/main.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main(){
-  runApp(Center(
-      child: Text(
-        'Hola mundito', textDirection: TextDirection.ltr,
+  runApp(MyApp());//Center
+}
+
+
+class MyApp extends StatefulWidget{
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  MaterialApp(
+      title: 'Mi premieŕe app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Primera Version"),
+          backgroundColor: Color.fromARGB(122, 90, 233, 123),
+        ),
+        body: Container(
+            child: Center(
+                child: Text("Hola mundo")
+            )
+        ),
       ),
-  )
-  );//Center
+    );
+  }
+
 }
 
